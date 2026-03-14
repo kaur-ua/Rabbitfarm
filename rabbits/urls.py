9fd5c2025d570616ee4c4821c5a74e4bc5c550af
Yurii Kazmirchuk <uyriy2007@gmail.com>
2026-03-14 16:56:29 +0200

    Added colored event statuses and journal improvements

--- rabbits/urls.py
+++ rabbits/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.rabbit_list, name="rabbit_list"),
-    path("int:pk/", views.rabbit_detail, name="rabbit_detail"),
+    path("<int:pk>/", views.rabbit_detail, name="rabbit_detail")
+
+    ]
     
-]
+
 
 
