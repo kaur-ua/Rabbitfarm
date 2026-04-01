7358e3feff1381afa87042b6e967e9dd00b5a1ca
kaur-ua <uyriy2007@gmail.com>
2026-04-01 15:48:25 +0300

    Add automatic group creation after weaning

--- events/views.py
+++ events/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from datetime import timedelta
 from .forms import EventForm
 from farms.models import Farm
+from rabbits.models import Group
 
 
 def add_event(request):
@@ -9,20 +10,35 @@ def add_event(request):
         form = EventForm(request.POST)
         if form.is_valid():
             event = form.save(commit=False)
+            
 
             if event.event_type == "mating":
                 event.next_action = "Очікуваний окрол"
                 event.next_action_date = event.date + timedelta(days=28)
 
-            elif event.event_type == "birth":
-                event.next_action = "Відлучення"
+            elif event.event_type == "kindling":
+                event.next_action = "Відсадка"
                 event.next_action_date = event.date + timedelta(days=60)
 
             elif event.event_type == "vaccination":
                 event.next_action = "Наступна вакцинація"
                 event.next_action_date = event.date + timedelta(days=180)
 
+            elif event.event_type == "weaning":
+                event.next_action = "Групу створено"
             event.save()
+
+            if event.event_type == "weaning":
+                farm = request.user.farms.first()
+
+                Group.objects.get_or_create(
+                   name=f"{event.rabbit.name}_{event.date}",
+                   farm=farm,
+                   defaults={
+                       "description": f"Створено після відсадки від {event.rabbit.name}"
+                }
+                )
+
             return redirect("home")
 
     else:
