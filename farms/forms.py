e07b8e78205895854611faf6b6a58c0871dc17dd
Yurii Kazmirchuk <uyriy2007@gmail.com>
2026-08-27 13:00:21 +0300

    Complete Ukrainian localization for farm pages

--- farms/forms.py
+++ farms/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
+
 from .models import Farm
 
 
@@ -6,3 +8,11 @@ class FarmForm(forms.ModelForm):
     class Meta:
         model = Farm
         fields = ['name', 'location', 'phone', 'description', 'image']
+
+        labels = {
+            'name': _('Farm Name'),
+            'location': _('Location'),
+            'phone': _('Phone'),
+            'description': _('Description'),
+            'image': _('Farm Photo'),
+        }
